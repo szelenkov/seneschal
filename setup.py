8d145333e5a6a8a20fe7aeee848b2e8c6deabdeb
Serhiy Zelenkov <zelenkov@gmail.com>
2021-03-28 11:53:36 -0700

    setup update

--- setup.py
+++ setup.py
@@ -1,8 +1,24 @@
-#!python -u
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python -u
+# -*- mode: python; coding: utf-8 -*-
 #
-# pylama: ignore:E265
+# pylama: ignore:E2651
 """
+Setup script for seneschal application.
+
+Copyright (C) Serhiy Zelenkov
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 Setup.
 
     pip install -e . -U -r requirements.txt
@@ -12,6 +28,20 @@ https://packaging.python.org/en/latest/distributing.html
 
 Excellent guide "Less known packaging features and tricks"
     https://blog.ionelmc.ro/2014/05/25/python-packaging/
+
+Usage:
+--------------------------------------------------------------------------
+    to build package:       python setup.py build
+    to install package:     python setup.py install
+    to remove installation: python setup.py uninstall
+--------------------------------------------------------------------------
+    to create source distribution:   python setup.py sdist
+--------------------------------------------------------------------------
+    to create binary RPM distribution:  python setup.py bdist_rpm
+--------------------------------------------------------------------------
+    to create binary DEB distribution:  python setup.py bdist_deb
+--------------------------------------------------------------------------.
+    Help on available distribution formats: --help-formats
 """
 from glob import glob
 import sys
@@ -27,17 +57,19 @@ from setuptools.command.test import test as test_command
 
 
 #%%
-__author__ = "Serhiy Zelenkov"
-__copyright__ = "Copyright 2019"
-__credits__ = []   # type: list
-__license__ = "MIT License"
-__version__ = "0.0.1"
+__copyright__ = __doc__.split('\n')[3]
+__author__ = ' '.join(__copyright__.split()[-2:])
+__credits__ = []  # type: list
+__license__ = 'MIT License'
 __maintainer__ = __author__
-__email__ = "zelenkov@gmail.com"
-__status__ = "Production"
+__email__ = 'zelenkov@gmail.com'
+__status__ = 'Protoduction'
+__version_info__ = (0, 0, 1)
+__version__ = '.'.join(map(str, __version_info__))
+
 #%%
 REQUIRED_PYTHON_VER = (3, 6, 5)
-GITHUB_URL = "https://github.com/szelenkov/seneschal"
+GITHUB_URL = 'https://github.com/szelenkov/seneschal'
 HERE = abspath(dirname(__file__))
 
 
@@ -49,17 +81,17 @@ class PyTest(test_command):
         test_command
     """
 
-    user_options = [("pytest-args=", "a", "Arguments to pass to pytest")]
+    user_options = [('pytest-args=', 'a', 'Arguments to pass to pytest')]
 
     def __init__(self, dist, **kw):
         """Initialize."""
         super().__init__(dist, **kw)
-        self.pytest_args = ""
+        self.pytest_args = ''
 
     def initialize_options(self):
         """Initialize options."""
         test_command.initialize_options(self)
-        self.pytest_args = ""
+        self.pytest_args = ''
 
     def run_tests(self):
         """Import here, cause outside the eggs aren't loaded."""
@@ -67,8 +99,8 @@ class PyTest(test_command):
         import shlex
         import pytest
 
-        errno = pytest.main(shlex.split(self.pytest_args))
-        sys.exit(errno)
+        error_no = pytest.main(shlex.split(self.pytest_args))
+        sys.exit(error_no)
 
 
 def clean():
@@ -76,75 +108,77 @@ def clean():
     print(clean.__doc__)
     root = dirname(realpath(__file__))
     for directory in ['build', 'dist', 'seneschal.egg-info', '.eggs']:
-        dpath = join(root, directory)
-        if isdir(dpath):
-            rmtree(dpath)
+        dir_path = join(root, directory)
+        if isdir(dir_path):
+            rmtree(dir_path)
 
 
-clean()
+if __name__ == '__main__':
+    clean()
 
-
-with open("README.md", "r") as fh:
-    # https://www.python.org/dev/peps/pep-0345/#fields
-    setup(author=__author__,
-          author_email=__email__,
-          # https://www.python.org/dev/peps/pep-0301/
-          classifiers=[
-              'Development Status :: 4 - Beta',
-              'Intended Audience :: End Users/Desktop',
-              'Intended Audience :: Developers',
-              'License :: OSI Approved :: MIT License',
-              'Natural Language :: English',
-              'Operating System :: MacOS',
-              'Operating System :: Microsoft :: Windows',
-              'Operating System :: POSIX :: Linux',
-              'Programming Language :: Python',
-              'Topic :: Software Development',
-              'Topic :: Text Editors',
-              'Topic :: Text Processing',
-          ],
-          cmdclass={'install': clean, 'pytest': PyTest},
-          entry_points={
-              'console_scripts': [
-                  'seneschal = seneschal:main'
-              ],
-          },
-          # becomes 'Summary' in pkg-info
-          description='An IDE, PIM and Outliner',
-          download_url='http://leoeditor.com/download.html',
-          # also include MANIFEST files in wheels
-          include_package_data=True,
-          install_requires=[
-              'docutils',  # used by Sphinx, rST plugin
-              'pylint', 'pyflakes',  # coding syntax standards
-              'pypandoc',  # doc format conversion
-              'sphinx',  # rST plugin
-              'semantic_version',  # Pip packaging
-              'twine', 'wheel', 'keyring'  # Pip packaging, uploading to PyPi
-          ],
-          keywords='seneschal',
-          license=__license__,
-          long_description=fh.read(),
-          long_description_content_type="text/markdown",
-          name='seneschal',
-          packages=find_packages(),
-          package_dir={'': '.'},
-          platforms=['Linux', 'Windows', 'MacOS'],
-          project_urls={
-              'Bug Reports': f'{GITHUB_URL}/issues',
-              'Dev Docs': 'https://developers.home-assistant.io/',
-              'Discord': 'https://discordapp.com/invite/c5DvZ4e',
-              'Forum': 'https://community.home-assistant.io/'
-          },
-          py_modules=[splitext(basename(path))[0]
-                      for path in glob('src/*.py')],
-          python_requires=f'>={".".join(map(str, REQUIRED_PYTHON_VER))}',
-          # semantic_version here to force download and making available
-          # before installing Leo Is also in `user_requires` so pip installs
-          # it too for general use
-          setup_requires=['semantic_version', 'pytest-runner'],
-          tests_require=['pytest'],
-          url='https://www.python.org/sigs/distutils-sig/',
-          version=__version__,
-          zip_safe=False
-          )
+    with open('README.md', 'r') as fh:
+        with open('requirements.txt') as f:
+            # https://www.python.org/dev/peps/pep-0345/#fields
+            setup(author=__author__,
+                  author_email=__email__,
+                  # https://www.python.org/dev/peps/pep-0301/
+                  classifiers=[
+                      'Development Status :: 4 - Beta',
+                      'Environment :: Console',
+                      'Intended Audience :: End Users/Desktop',
+                      'Intended Audience :: Developers',
+                      'Libraries :: Python Modules',
+                      'License :: OSI Approved :: MIT License',
+                      'Natural Language :: English',
+                      'Operating System :: OS Independent',
+                      'Programming Language :: Python',
+                      'Programming Language :: Python :: 3',
+                      'Programming Language :: Python :: 3.7',
+                      'Programming Language :: Python :: 3.8',
+                      'Programming Language :: Python :: 3.9',
+                      'Programming Language :: Python :: 3.10',
+                      'Programming Language :: Python :: Implementation :: CPython',
+                      'Programming Language :: Python :: Implementation :: PyPy',
+                      'Topic :: Software Development',
+                      'Topic :: Text Editors',
+                      'Topic :: Text Processing',
+                      'Topic :: Utilities',
+                  ],
+                  cmdclass={'install': clean, 'pytest': PyTest},
+                  # becomes 'Summary' in pkg-info
+                  description=__doc__.split('\n')[1],
+                  download_url=f'{GITHUB_URL}/download.html',
+                  entry_points={
+                      'console_scripts': ('seneschal = seneschal:main'),
+                  },
+                  extras_require={'test': ('pytest', 'coverage', ), },
+                  # also include MANIFEST files in wheels
+                  include_package_data=True,
+                  install_requires=f.read().splitlines(),
+                  keywords='seneschal',
+                  license=__license__,
+                  long_description=fh.read(),
+                  long_description_content_type='text/markdown',
+                  name='seneschal',
+                  packages=find_packages(),
+                  package_data={},
+                  package_dir={'': '.'},
+                  platforms=['Linux', 'Windows', 'MacOS'],
+                  # List additional URLs that are relevant to project as a dict.
+                  project_urls={
+                      'Bug Reports': f'{GITHUB_URL}/issues',
+                      'Dev Docs': f'{GITHUB_URL}/devdoc'
+                  },
+                  py_modules=[splitext(basename(path))[0]
+                              for path in glob('src/*.py')],
+                  python_requires=f'>={".".join(map(str, REQUIRED_PYTHON_VER))}',
+                  # semantic_version here to force download and making available
+                  # before installing Leo Is also in `user_requires` so pip installs
+                  # it too for general use
+                  setup_requires=('semantic_version', 'setuptools >= 53.0.0', 'pytest-runner'),
+                  requires=f.read().splitlines(),
+                  tests_require=('pytest'),
+                  url=GITHUB_URL,
+                  version=__version__,
+                  zip_safe=False
+                  )
